553f6b3259d491498f5ab4f16a87fa00a9aff950
Leroy <leroy.chin@student.unsw.edu.au>
2026-03-15 15:39:59 +1100

    added supabase without an url or key

--- src/dataStore.ts
+++ src/dataStore.ts
@@ -1,4 +1,12 @@
 import { Order, Users, Session} from './interfaces';
+import { createClient } from '@supabase/supabase-js';
+
+
+// link to supabase, make sure to put your keys in .env
+const supabaseUrl = process.env.supabaseURL || '';
+const supabaseKey = process.env.supabaseKey || '';
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
 export interface Data {
     users: Users[];
